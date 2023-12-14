1c14a6e5aa08cdcf7403c01baaf4dc458112dbda
Eduard1151456 <eduardmauriciolh@ufps.edu.co>
2023-12-13 22:48:14 -0500

    pack

--- eclipse-workspace/taller/src/main/java/com/example/demo/entities/Producto.java
+++ eclipse-workspace/taller/src/main/java/com/example/demo/entities/Producto.java
@@ -1,5 +1,15 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+@Data
+@Entity
 public class Producto {
-
+	@Id
+	@Column(nullable=false, length=10)
+	 private Integer id;
+	@Column (nullable=false, length=20)private String name;
+	@Column (nullable=false, length=20)private double price;
 }
--- eclipse-workspace/taller/src/main/java/com/example/demo/entities/Proveedor.java
+++ eclipse-workspace/taller/src/main/java/com/example/demo/entities/Proveedor.java
@@ -1,5 +1,15 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+@Data
+@Entity
 public class Proveedor {
-
+	@Id
+	@Column(nullable=false, length=10)
+	private Long id;
+	@Column (nullable=false, length=20)String companyName;
+	@Column (nullable=false, length=20)String contactName;
 }
